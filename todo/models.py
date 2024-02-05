b71592f0cfe7eea1ffb2e6f6cb7b845385907123
Your_Name Your_Surname <your@email>
2024-02-05 13:00:38 +0200

    Created models Tag and Task

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    create_date = models.DateTimeField(auto_now_add=True)
+    dedline = models.DateTimeField(blank=True, null=True)
+    implementation = models.BooleanField()
+    tags = models.ManyToManyField(Tag, related_name="tasks")
