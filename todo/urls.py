7e10589614686253dfbdd448ea076bea067810e4
Your_Name Your_Surname <your@email>
2024-02-05 15:30:45 +0200

    Add Create- Update- Delete- View by Tag and add frontend by this pages

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from todo.views import HomePageView, TagListView, complete_task, undo_task, TaskCreateView, TaskUpdateView, \
-    TaskDeleteView
+    TaskDeleteView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
@@ -11,6 +11,9 @@ urlpatterns = [
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -44,3 +44,20 @@ class TaskUpdateView(generic.UpdateView):
 class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todo:index")
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
