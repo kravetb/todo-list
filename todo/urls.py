48ce44d209445e9a20d96e00f8535a837f948bc8
Your_Name Your_Surname <your@email>
2024-02-05 13:45:45 +0200

    Add view and template by tag_lis and change sidebar

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from todo.views import HomePageView
+from todo.views import HomePageView, TagListView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,11 @@
 from django.views import generic
 
+from todo.models import Tag
+
 
 class HomePageView(generic.base.TemplateView):
     template_name = "todo/index.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
