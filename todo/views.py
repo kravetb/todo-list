ab850016b68636b325baa0d37871b5617695bbff
Your_Name Your_Surname <your@email>
2024-02-06 20:02:59 +0200

    Change complete_task and undo_task

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
+from django.views.decorators.http import require_POST
 
 from todo.models import Tag, Task
 
@@ -15,6 +16,7 @@ class TagListView(generic.ListView):
     model = Tag
 
 
+@require_POST
 def complete_task(request, pk):
     task = get_object_or_404(Task, id=pk)
     task.implementation = True
@@ -22,6 +24,7 @@ def complete_task(request, pk):
     return HttpResponseRedirect(reverse_lazy("todo:index"))
 
 
+@require_POST
 def undo_task(request, pk):
     task = get_object_or_404(Task, id=pk)
     task.implementation = False
