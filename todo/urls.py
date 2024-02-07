90387534837559fa3780c98a6e00ade5727cfa66
Your_Name Your_Surname <your@email>
2024-02-07 20:36:50 +0200

    Make UndoTaskView and CompleteTaskView

--- todo/urls.py
+++ todo/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from todo.views import HomePageView, TagListView, complete_task, undo_task, TaskCreateView, TaskUpdateView, \
+from todo.views import HomePageView, TagListView, CompleteTaskView, UndoTaskView, TaskCreateView, TaskUpdateView, \
     TaskDeleteView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="index"),
     path("tags/", TagListView.as_view(), name="tag-list"),
-    path('complete_task/<int:pk>/', complete_task, name='complete_task'),
-    path('undo_task/<int:pk>/', undo_task, name='undo_task'),
+    path('complete_task/<int:pk>/', CompleteTaskView.as_view(), name='complete_task'),
+    path('undo_task/<int:pk>/', UndoTaskView.as_view(), name='undo_task'),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views import generic
+from django.views import generic, View
 from django.views.decorators.http import require_POST
 
 from todo.models import Tag, Task
@@ -16,20 +15,20 @@ class TagListView(generic.ListView):
     model = Tag
 
 
-@require_POST
-def complete_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    task.implementation = True
-    task.save()
-    return HttpResponseRedirect(reverse_lazy("todo:index"))
+class CompleteTaskView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+        task.implementation = True
+        task.save()
+        return redirect("todo:index")
 
 
-@require_POST
-def undo_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    task.implementation = False
-    task.save()
-    return HttpResponseRedirect(reverse_lazy("todo:index"))
+class UndoTaskView(View):
+    def post(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+        task.implementation = False
+        task.save()
+        return redirect("todo:index")
 
 
 class TaskCreateView(generic.CreateView):
